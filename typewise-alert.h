ed14c9cff280f8c4d50124280fcaee080a9238e7
ndt1cob <dhanakumar.nithinveerakumar@in.bosch.com>
2023-01-10 22:34:51 +0530

    comit 9

--- typewise-alert.h
+++ typewise-alert.h
@@ -8,7 +8,3 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 typedef void (*alertMedium)(BreachType);
-
-map<CoolingType,Limit> CoolingInfo;
-map<AlertTarget,alertMedium> AlertInfo;
-map<BreachType,string> messageForBreachType;
